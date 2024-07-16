f001c5adfb0007fcc86266d12037776e569866c0
Reece-AvoChoc <reece@avochoc.com>
2024-07-16 08:41:40 +0200

    Initial commit

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomePage } from './pages/home/home.page';
+import { AboutPage } from './pages/about/about.page';
+
+const routes: Routes = [
+    { path: 'home', component: HomePage },
+    { path: 'about', component: AboutPage },
+    { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomePage,
+        AboutPage
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+        // Add other imported modules here
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
