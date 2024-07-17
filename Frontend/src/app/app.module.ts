137e837a5afe6aa186f4b853436ae71720b6a35a
Dominique Grobler <dominique@avochoc.com>
2024-07-17 08:31:08 +0200

    Update page styles, add modal for joining, and handle form submission

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { HomePage } from './pages/home/home.page';
-import { AboutPage } from './pages/about/about.page';
-import { provideHttpClient } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HomePage } from "./pages/home/home.page";
+import { AboutPage } from "./pages/about/about.page";
+import { provideHttpClient } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 const routes: Routes = [
-  { path: 'home', component: HomePage },
-  { path: 'about', component: AboutPage },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: "home", component: HomePage },
+  { path: "about", component: AboutPage },
+  { path: "", redirectTo: "/home", pathMatch: "full" }, // Default route
 ];
 
 @NgModule({
   declarations: [AppComponent, HomePage, AboutPage],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), ReactiveFormsModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
--- Frontend/src/app/pages/about/about.page.ts
+++ Frontend/src/app/pages/about/about.page.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { BackendService } from "../../../services/backend.service";
 import { AboutModel } from "./about-us.model";
 
 @Component({
   selector: "app-about",
   templateUrl: "about.page.html",
-  styleUrls: [],
 })
 export class AboutPage {
   mainHeading: string = "StrumFusion";
@@ -28,12 +28,67 @@ export class AboutPage {
     "Connect with fellow guitarists from around the globe. Our forums, social media channels, and live events provide a platform for you to share your experiences, ask questions, and get feedback.";
   joinText: string =
     "Whether you're picking up a guitar for the first time or looking to enhance your skills, StrumFusion is here to support you every step of the way. Join our community, explore our resources, and let your guitar journey begin!";
+
+  isModalOpen: boolean = false;
+  name: string = "";
+  surname: string = "";
+  email: string = "";
+  password: string = "";
+  showError: boolean = false;
+  showWelcomePopup: boolean = false;
+  welcomeMessage: string = "";
+  joinMessage: string = "";
+
   aboutData: AboutModel = {
     id: "0",
     text: "",
   };
 
-  constructor(private backendService: BackendService) {}
+  joinForm: FormGroup;
+
+  constructor(
+    private backendService: BackendService,
+    private formBuilder: FormBuilder
+  ) {
+    this.joinForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      surname: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.showError = false;
+    console.log("Modal closed");
+  }
+
+  submitForm() {
+    if (this.joinForm.invalid) {
+      this.showError = true;
+      console.log("Form invalid");
+      return;
+    }
+
+    const fullName = `${this.name} ${this.surname}`;
+
+    this.welcomeMessage = "Welcome to StrumFusion, John Doe!";
+    this.joinMessage =
+      "We're thrilled to have you on board. You're now part of a global community of guitar enthusiasts who share your passion for music. Get ready to learn, connect, and grow with us. The journey starts now!";
+
+    this.closeModal();
+
+    setTimeout(() => {
+      this.showWelcomePopup = true;
+    }, 300);
+
+    this.joinForm.reset();
+  }
 
   ngOnInit() {
     this.backendService.getAbout().subscribe({
