61400f290d2bb3e4726f0d94e790c6cd3390417a
Tyler-dev-eng <tbrecht17@gmail.com>
2024-07-18 10:41:36 +0200

    Implemented 404 page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
-import { HomePage } from "./pages/home/home.page";
-import { AboutPage } from "./pages/about/about.page";
-import { provideHttpClient } from "@angular/common/http";
-import { ReactiveFormsModule } from "@angular/forms";
-import { GetInTouchPopupComponent } from "./components/get-in-touch-popup/get-in-touch-popup.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HomePage } from './pages/home/home.page';
+import { AboutPage } from './pages/about/about.page';
+import { provideHttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GetInTouchPopupComponent } from './components/get-in-touch-popup/get-in-touch-popup.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 const routes: Routes = [
-  { path: "home", component: HomePage },
-  { path: "about", component: AboutPage },
-  { path: "", redirectTo: "/home", pathMatch: "full" }, // Default route
+  { path: 'home', component: HomePage },
+  { path: 'about', component: AboutPage },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
   declarations: [AppComponent, HomePage, AboutPage],
-  imports: [BrowserModule, RouterModule.forRoot(routes), ReactiveFormsModule, GetInTouchPopupComponent],
-  providers: [provideHttpClient()],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    GetInTouchPopupComponent,
+  ],
+  providers: [provideHttpClient(), RouterModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
