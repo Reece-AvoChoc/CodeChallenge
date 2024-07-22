3d655f3f8c9a875483e153311d3fb733585f473e
Dominique Grobler <dominique@avochoc.com>
2024-07-22 10:46:06 +0200

    Refactor profile component

--- Frontend/src/app/pages/profile/profile.component.ts
+++ Frontend/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UserModel } from "../../models/user.model";
 
 @Component({
-  selector: 'app-profile',
+  selector: "app-profile",
   standalone: true,
   imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.css",
 })
 export class ProfileComponent {
+  user?: UserModel;
+  backendService: any;
+  router: any;
 
+  getUserInitials(): string | undefined {
+    if (this.user?.email) {
+      const initials = this.user.email[0].toUpperCase();
+      return initials;
+    }
+    return undefined;
+  }
+
+  deleteUser() {
+    this.backendService.deleteUser(this.user?.email).subscribe({
+      next: () => {
+        this.router.navigateByUrl("/login");
+      },
+    });
+  }
 }
