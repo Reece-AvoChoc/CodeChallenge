f87a7378a5077a850dc35b271ff8b27f03fe016d
KenrickAvoc <kenrick@avochoc.com>
2024-07-25 10:37:45 +0200

    Edit User

--- Frontend/src/app/edit-user/edit-user.component.ts
+++ Frontend/src/app/edit-user/edit-user.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-user.component.html',
-  styleUrl: './edit-user.component.css'
+  styleUrls: ['./edit-user.component.css'],
 })
-export class EditUserComponent {
+export class EditUserModalComponent {
+  @Input() user: any;
+  @Output() save = new EventEmitter<any>();
+  @Output() close = new EventEmitter<void>();
 
+  closeModal() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.save.emit(this.user);
+    this.closeModal();
+  }
 }
--- Frontend/src/app/user-list/user-list.component.ts
+++ Frontend/src/app/user-list/user-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { User } from '../models/user';
 import { CommonModule } from '@angular/common';
+import { EditUserModalComponent } from '../edit-user/edit-user.component';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EditUserModalComponent],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css',
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent {
   testUsers: User[] = [
@@ -24,4 +25,22 @@ export class UserListComponent {
       password: 'password',
     },
   ];
+
+  selectedUser: User | null = null;
+
+  openModal(user: User) {
+    this.selectedUser = { ...user };
+  }
+
+  saveUser(user: User) {
+    const index = this.testUsers.findIndex((u) => u.id === user.id);
+    if (index !== -1) {
+      this.testUsers[index] = user;
+    }
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.selectedUser = null;
+  }
 }
